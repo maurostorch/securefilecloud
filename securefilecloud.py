b5323576bfadae2c030772844db7ee8c41617aea
Mauro Storch <mauro@derstorch.com>
2013-09-13 12:27:08 -0300

    download mode and simple shell

--- securefilecloud.py
+++ securefilecloud.py
@@ -1,5 +1,6 @@
 import dropbox
 import rsa
+import shutil
 import sys
 import tempfile
 
@@ -36,6 +37,50 @@ def encrypt(filepath):
 	tmp.close()
 	return (e,d,n,tmpfile)
 
+def download(client, filepath, d, n):
+	print 'downloading file: '+filepath
+	out = file('./'+filepath[filepath.rfind('/')+1:],'w')
+	f = client.get_file(filepath).read()
+	out.write(f)
+	out.close()
+	decrypt(out.name,d,n)
+
+def decrypt(filepath, d, n):
+	tmp = tempfile.NamedTemporaryFile(delete=False)
+	tmp.close()
+	shutil.copy(filepath, tmp.name)
+	tmp = open(tmp.name)
+	c = tmp.read()
+	f = open(filepath, 'w')
+	f.write(rsa.decrypt(c,d,n))
+	f.close()
+	tmp.close()
+
+def listfiles(client, directory):
+	meta = 	client.metadata(directory)
+	print meta
+	#for item in meta.contents:
+	#	print item.path
+
 if __name__ == "__main__":
 	client = connect(app_key,app_secret,'')
-	upload(client, sys.argv[1])
+	level = '/'
+	while True:
+		command = raw_input('upload <file> | download <file> key mod | list | cd | exit:').strip()
+		if command.split(' ')[0] == 'upload':
+			upload(client, command.split(' ')[1])
+		elif command.split(' ')[0] == 'download':
+			download(client,command.split(' ')[1],long(command.split(' ')[2]),long(command.split(' ')[3]))
+		elif command.split(' ')[0] == 'list':
+			listfiles(client,level)
+		elif command.split(' ')[0] == 'cd':
+			if command.split(' ')[1] == '..':
+				if level == '/': print '/'
+				else:
+					level = level[:level[:len(level)-1].rfind('/')+1]
+			else:
+				level = level+command.split(' ')[1]+'/'
+		elif command.split(' ')[0] == 'exit':
+			break
+		elif command.split(' ')[0] == 'pwd':
+			print level
